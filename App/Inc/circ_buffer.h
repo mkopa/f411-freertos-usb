8d6f1abd74dff7934531b8695ee112e948c6bbe8
Marcin Kopa <marcin.info@gmail.com>
2025-07-11 10:46:17 +0200

    Update meson.build

--- App/Inc/circ_buffer.h
+++ App/Inc/circ_buffer.h
@@ -1,15 +1,15 @@
 #ifndef __CIRC_BUFFER_H
 #define __CIRC_BUFFER_H
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #define CIRC_BUFFER_SIZE 256 // Możesz dostosować rozmiar
 
 typedef struct {
-    uint8_t buffer[CIRC_BUFFER_SIZE];
-    volatile uint16_t head;
-    volatile uint16_t tail;
+  uint8_t buffer[CIRC_BUFFER_SIZE];
+  volatile uint16_t head;
+  volatile uint16_t tail;
 } circ_buffer_t;
 
 void circ_buffer_init(circ_buffer_t *cb);
